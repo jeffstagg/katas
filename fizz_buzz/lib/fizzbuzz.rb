2f98e20cb878907b50db1b0837dabe0084fdcef8
Jeff Stagg <jeff.a.stagg@gmail.com>
2014-03-04 02:01:33 -0600

    adding fizzbuzz kata

---
+++ fizz_buzz/lib/fizzbuzz.rb
@@ -0,0 +1,28 @@
+class FizzBuzz
+
+	def convert(start, finish)
+		
+		result = ''
+		
+		(start..finish).each do |num|
+			result << fizzle(num) + " "
+		end
+		result.strip!
+		
+		return result
+	end
+
+	def fizzle(num)
+		
+		if (num % 3 == 0) || (num % 5 == 0)
+			result = ''
+			result << (num % 3 == 0 ? 'Fizz' : '')
+			result << (num % 5 == 0 ? 'Buzz' : '')
+			return result
+		end
+
+		return num.to_s
+	end
+
+end
+
