25f5a225a5a94717a6cdef84e404ba3988bb2d8e
Jeff Stagg <jeff.a.stagg@gmail.com>
2014-03-04 02:10:37 -0600

    formatting roman numerals

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,29 +1,29 @@
 class RomanNumeralsConverter
-	
-	CONVERSION_TABLE = [
-		[100,'C'],
-		[90,'XC'],
-		[50,'L'],
-		[40,'XL'],
-		[10,'X'],
-		[9,'IX'],
-		[5,'V'],
-		[4,'IV'],
-		[1,'I']
-	]
+  
+  CONVERSION_TABLE = [
+    [100,'C'],
+    [90,'XC'],
+    [50,'L'],
+    [40,'XL'],
+    [10,'X'],
+    [9,'IX'],
+    [5,'V'],
+    [4,'IV'],
+    [1,'I']
+  ]
 
-	def convert num
+  def convert num
 
-		result = ''
+    result = ''
 
-		CONVERSION_TABLE.each do |number,glyph|
-			while num >= number
-				result << glyph
-				num -= number
-			end
-		end
-		
-		result
-	
-	end
+    CONVERSION_TABLE.each do |number,glyph|
+      while num >= number
+        result << glyph
+        num -= number
+      end
+    end
+    
+    result
+  
+  end
 end
